d9c290e2da99edc99c90128bfaa3209ba453f7bd
Михаил Ефремов <seen.94@mail.ru>
2015-09-12 13:36:18 +0300

    Add uploaing picture and get there RGB info from pixel

--- pouring_picture/Form1.Designer.cs
+++ pouring_picture/Form1.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imegeUploadButton = new System.Windows.Forms.Button();
+            this.infoLabelRed = new System.Windows.Forms.Label();
+            this.infoLabelGreen = new System.Windows.Forms.Label();
+            this.infoLabelBlue = new System.Windows.Forms.Label();
+            this.labelRed = new System.Windows.Forms.Label();
+            this.labelGreen = new System.Windows.Forms.Label();
+            this.labelBlue = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(513, 455);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // imegeUploadButton
+            // 
+            this.imegeUploadButton.Location = new System.Drawing.Point(531, 443);
+            this.imegeUploadButton.Name = "imegeUploadButton";
+            this.imegeUploadButton.Size = new System.Drawing.Size(102, 23);
+            this.imegeUploadButton.TabIndex = 1;
+            this.imegeUploadButton.Text = "Upload image";
+            this.imegeUploadButton.UseVisualStyleBackColor = true;
+            this.imegeUploadButton.Click += new System.EventHandler(this.imegeUploadButton_Click);
+            // 
+            // infoLabelRed
+            // 
+            this.infoLabelRed.AutoSize = true;
+            this.infoLabelRed.Location = new System.Drawing.Point(531, 59);
+            this.infoLabelRed.Name = "infoLabelRed";
+            this.infoLabelRed.Size = new System.Drawing.Size(30, 13);
+            this.infoLabelRed.TabIndex = 2;
+            this.infoLabelRed.Text = "Red:";
+            // 
+            // infoLabelGreen
+            // 
+            this.infoLabelGreen.AutoSize = true;
+            this.infoLabelGreen.Location = new System.Drawing.Point(531, 72);
+            this.infoLabelGreen.Name = "infoLabelGreen";
+            this.infoLabelGreen.Size = new System.Drawing.Size(40, 13);
+            this.infoLabelGreen.TabIndex = 3;
+            this.infoLabelGreen.Text = "Green:";
+            // 
+            // infoLabelBlue
+            // 
+            this.infoLabelBlue.AutoSize = true;
+            this.infoLabelBlue.Location = new System.Drawing.Point(531, 85);
+            this.infoLabelBlue.Name = "infoLabelBlue";
+            this.infoLabelBlue.Size = new System.Drawing.Size(31, 13);
+            this.infoLabelBlue.TabIndex = 4;
+            this.infoLabelBlue.Text = "Blue:";
+            // 
+            // labelRed
+            // 
+            this.labelRed.AutoSize = true;
+            this.labelRed.Location = new System.Drawing.Point(578, 59);
+            this.labelRed.Name = "labelRed";
+            this.labelRed.Size = new System.Drawing.Size(0, 13);
+            this.labelRed.TabIndex = 5;
+            // 
+            // labelGreen
+            // 
+            this.labelGreen.AutoSize = true;
+            this.labelGreen.Location = new System.Drawing.Point(578, 72);
+            this.labelGreen.Name = "labelGreen";
+            this.labelGreen.Size = new System.Drawing.Size(0, 13);
+            this.labelGreen.TabIndex = 6;
+            // 
+            // labelBlue
+            // 
+            this.labelBlue.AutoSize = true;
+            this.labelBlue.Location = new System.Drawing.Point(578, 85);
+            this.labelBlue.Name = "labelBlue";
+            this.labelBlue.Size = new System.Drawing.Size(0, 13);
+            this.labelBlue.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 479);
+            this.Controls.Add(this.labelBlue);
+            this.Controls.Add(this.labelGreen);
+            this.Controls.Add(this.labelRed);
+            this.Controls.Add(this.infoLabelBlue);
+            this.Controls.Add(this.infoLabelGreen);
+            this.Controls.Add(this.infoLabelRed);
+            this.Controls.Add(this.imegeUploadButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button imegeUploadButton;
+        private System.Windows.Forms.Label infoLabelRed;
+        private System.Windows.Forms.Label infoLabelGreen;
+        private System.Windows.Forms.Label infoLabelBlue;
+        private System.Windows.Forms.Label labelRed;
+        private System.Windows.Forms.Label labelGreen;
+        private System.Windows.Forms.Label labelBlue;
     }
 }
 
--- pouring_picture/Form1.cs
+++ pouring_picture/Form1.cs
@@ -16,5 +16,45 @@ namespace pouring_picture
         {
             InitializeComponent();
         }
+
+        private void imegeUploadButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            UploadImage();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            Point coordinates = me.Location;
+
+            GetBitMap(coordinates);
+        }
+
+        private void UploadImage()
+        {
+            OpenFileDialog open = new OpenFileDialog();
+
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(open.FileName);
+            }
+        }
+
+        private void GetBitMap(Point point)
+        {
+            using (Bitmap bmp = new Bitmap(pictureBox1.Image))
+            {
+                Color clr = bmp.GetPixel(point.X, point.Y);
+                int red = clr.R;
+                int green = clr.G;
+                int blue = clr.B;
+
+                labelRed.Text = red.ToString();
+                labelGreen.Text = green.ToString();
+                labelBlue.Text = blue.ToString();
+            }
+        }
     }
 }
