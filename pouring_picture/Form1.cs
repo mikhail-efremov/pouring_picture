262f6d1a6dbc4b236ef3f0812283060183e75dfe
Mikhail Efremov <seen.94@mail.ru>
2015-12-14 21:11:25 +0200

    Delete PickRandomBrush()

--- pouring_picture/Form1.cs
+++ pouring_picture/Form1.cs
@@ -520,22 +520,6 @@ namespace pouring_picture
             GetColor();
         }
 
-        private Brush PickRandomBrush()
-        {
-            Brush result = Brushes.Transparent;
-
-            Random rnd = new Random();
-
-            Type brushesType = typeof(Brushes);
-
-            PropertyInfo[] properties = brushesType.GetProperties();
-
-            int random = rnd.Next(properties.Length);
-            result = (Brush)properties[random].GetValue(null, null);
-
-            return result;
-        }
-
         private void buttonDraw_Click(object sender, EventArgs e)
         {
             if (pixelDatas.Count != 0)
